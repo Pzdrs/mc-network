36eb4b510b8a6fcb02bb48257918e9073496d302
Petr Boháč <petrbohac3@seznam.cz>
2026-03-20 21:56:28 +0100

    feat: scaffold lobby command

--- lobby-server/src/main/java/cz/pycrs/lobbyserver/commands/LobbyCommand.java
+++ lobby-server/src/main/java/cz/pycrs/lobbyserver/commands/LobbyCommand.java
@@ -1,14 +1,35 @@
 package cz.pycrs.lobbyserver.commands;
 
+import net.minestom.server.command.CommandSender;
 import net.minestom.server.command.builder.Command;
+import net.minestom.server.command.builder.CommandContext;
+import net.minestom.server.command.builder.arguments.ArgumentType;
+import net.minestom.server.command.builder.arguments.number.ArgumentInteger;
 
 public class LobbyCommand extends Command {
     public LobbyCommand() {
         super("lobby");
 
-        setDefaultExecutor((sender, ctx) -> {
-            sender.sendMessage("Lobby command!");
-        });
+        setDefaultExecutor(this::onDefault);
+
+        var listArg = ArgumentType.Literal("list");
+        addSyntax(this::onList, listArg);
+
+        ArgumentInteger lobbyArg = ArgumentType.Integer("lobby");
+        addSyntax(this::onSendSpecific, ArgumentType.Literal("send"), lobbyArg);
 
     }
+
+    private void onDefault(CommandSender sender, CommandContext ctx) {
+        sender.sendMessage("Sending to random lobby");
+    }
+
+    private void onList(CommandSender sender, CommandContext ctx) {
+        sender.sendMessage("Listing lobbies");
+    }
+
+    private void onSendSpecific(CommandSender sender, CommandContext ctx) {
+        sender.sendMessage("Sending to lobby " + ctx.get("lobby"));
+    }
+
 }
